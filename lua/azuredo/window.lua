7b710e32af963c4b7ec0871981ae435110d59bcb
Andreas Olsson <olsson.andreas@gmail.com>
2025-02-09 13:13:44 +0100

    Some adjustments

--- lua/azuredo/window.lua
+++ lua/azuredo/window.lua
@@ -9,24 +9,32 @@ function M.createTelescopeWindow(options, callback)
   local finders = require("telescope.finders")
   local conf = require("telescope.config").values
 
+  local max_height = 40
+  local height = math.min(#options * 5, max_height)
   pickers
-    .new({}, {
-      prompt_title = "Select an Option",
-      finder = finders.new_table({
-        results = options,
-      }),
-      sorter = conf.generic_sorter({}),
-      attach_mappings = function(_, map)
-        map("i", "<CR>", function(prompt_bufnr)
-          local selection = require("telescope.actions.state").get_selected_entry()
-          require("telescope.actions").close(prompt_bufnr)
-          Util.debug(selection.index)
-          callback(selection.index)
-        end)
-        return true
-      end,
-    })
-    :find()
+      .new({}, {
+        prompt_title = "Select an Option",
+        finder = finders.new_table({
+          results = options,
+        }),
+        sorter = conf.generic_sorter({}),
+        layout_strategy = "center",
+        layout_config = {
+          width = 80,
+          height = height,
+          prompt_position = "bottom"
+        },
+        attach_mappings = function(_, map)
+          map("i", "<CR>", function(prompt_bufnr)
+            local selection = require("telescope.actions.state").get_selected_entry()
+            require("telescope.actions").close(prompt_bufnr)
+            Util.debug(selection.index)
+            callback(selection.index)
+          end)
+          return true
+        end,
+      })
+      :find()
 end
 
 ---@param options string[]
